3413f23eb3ade89e069ade29c88d3747231337e9
Harshit-875 <harshitgupta0024@gmail.com>
2025-09-13 17:34:55 +0530

    removing comment from index.js

--- Backend/index.js
+++ Backend/index.js
@@ -12,18 +12,11 @@ app.use(express.json())
 app.use(cors())
 
 // Available Routes
-// app.use('/api/auth', require('./routes/auth.js'))
-// app.use('/api/notes', require('./routes/notes.js'))
-
-// Available Routes
 const authRoutes=require('./routes/auth.js')
 const notesRoutes=require('./routes/notes.js')
 app.use('/api/auth', authRoutes)
 app.use('/api/notes', notesRoutes)
 
-app.get('/harshit',(req,res)=>{
-    res.send("Harshit is handsome always")
-})
 
 app.listen(port, () => {
     console.log("Server listening")
